25f574a65678dce0f31d71db642cbecc6c27160d
ZhengmaoOuyang <ouy.zhengmao@gmail.com>
2023-06-25 02:46:43 -0400

    website now loads landing page upon startup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import ViewUni from "./components/ViewUni";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Landing />} />
           <Route exact path="/index" element={<Home />} />
           <Route exact path="/get_uni/:name" element={<ViewUni />} />
         </Routes>
