67eec87d09a54e940d93a32be58e6a18aa65d5e3
aparajitaghimire <=>
2023-06-25 13:08:16 -0400

    implemented ticket 38: course filter ui

---
+++ src/components/CoursePageFilters.js
@@ -0,0 +1,101 @@
+import "./CoursePageFilters.css";
+import { Box, List, ListItem, ListItemButton } from "@mui/material";
+import React, { useState } from "react";
+import { ScrollView } from "react-native";
+
+function CoursePageFilters() {
+    const xchangeGold = "rgba(224, 208, 59, 0.54)";
+    const borderStyle = "1px solid #A8A8A8";
+
+    const programList = [[0, ["Software Engineering", "SE"]], [1,["Biomedical Engineering", "BME"]], [2, ["Civil Engineering", "CIVE"]], [3, ["Management Engineering", "MSCI"]], [4, ["Mechanical Engineering", "ME"]], [5, ["Computer Science", "CS"]]];
+    const programMap = new Map(programList);
+
+    const uniList = [[0,["National University of Singapore", "NUS"]], [1, ["Bilkent University", "BU"]], [2, ["Deakin University", "DU"]], [3, ["Ewha Womans University", "EWHA"]], [4, ["Griffith University", "GU"]], [5, ["University of Leeds", "LISS"]]];
+    const uniMap = new Map(uniList);
+    
+    const selectedPrograms = new Map();
+    const selectedUnis = new Map();
+
+    const [programs, setPrograms] = useState(programMap);
+    const [unis, setUnis] = useState(uniMap);
+    const [selectPrograms, setSelectedPrograms] = useState(selectedPrograms);
+    const [selectUnis, setSelectedUnis] = useState(selectedUnis);
+
+    function onProgramFilterClick(id) {
+        const newSelectedMap = new Map(selectPrograms);
+        const newProgramMap = new Map(programs);
+
+        if(programs.has(id)) {
+            newSelectedMap.set(id, programs.get(id))
+            newProgramMap.delete(id);
+        } else {
+            newProgramMap.set(id, selectPrograms.get(id));
+            newSelectedMap.delete(id);
+        }
+        setSelectedPrograms(newSelectedMap);
+        setPrograms(newProgramMap);
+    }
+
+    function onUniFilterClick(id) {
+        const newSelectedMap = new Map(selectUnis);
+        const newUniMap = new Map(unis);
+
+        if(unis.has(id)) {
+            newSelectedMap.set(id, unis.get(id));
+            newUniMap.delete(id);
+        } else {
+            newUniMap.set(id, selectUnis.get(id));
+            newSelectedMap.delete(id);
+        }
+        setSelectedUnis(newSelectedMap);
+        setUnis(newUniMap);
+    }
+
+    return(
+        <div className="CoursePageFilters">
+            <Box sx={{maxWidth: 350, maxHeight: 550, border:borderStyle, borderRadius:2}}>
+                <div className="ProgramFilter">
+                    <div className="FilterTitle"><h5>Program</h5></div>
+                    <ScrollView horizontal={true}>
+                        {[...selectPrograms.keys()].map((key) => 
+                            <div style={{paddingLeft:5, paddingTop:5}}>
+                                <ListItem sx={{background:xchangeGold, borderRadius:2}} onClick={() => {onProgramFilterClick(key)}}>
+                                    {selectPrograms.get(key)[1]}
+                                </ListItem>
+                            </div>
+                        )}
+                    </ScrollView>
+                    <List sx={{maxHeight: 175, overflow:"auto"}}>
+                        {[...programs.keys()].map((key) =>
+                            <ListItemButton onClick={() => {onProgramFilterClick(key)}}>
+                                {programs.get(key)[0]}
+                            </ListItemButton>
+                        )}
+                    </List>
+                </div>
+                <div style={{paddingBottom:10}}></div>
+                <div className="UniversityFilter" style={{borderTop:borderStyle, borderRadius:5}}>
+                    <div className="FilterTitle"><h5>University</h5></div>
+                    <ScrollView horizontal={true}>
+                        {[...selectUnis.keys()].map((key) => (
+                            <div style={{paddingLeft:5, paddingTop:5}}>
+                                <ListItem sx={{background:xchangeGold, borderRadius:3}} onClick={() => {onUniFilterClick(key)}}>
+                                    {selectUnis.get(key)[1]}
+                                </ListItem>
+                            </div>
+                        ))}
+                    </ScrollView>
+                    <List sx={{maxHeight: 175, overflow:"auto"}}>
+                        {[...unis.keys()].map((key) =>
+                            <ListItemButton onClick={() => {onUniFilterClick(key)}}>
+                                {unis.get(key)[0]}
+                            </ListItemButton>
+                        )}
+                    </List>
+                </div>
+            </Box>
+        </div>
+    );
+}
+
+export default CoursePageFilters;
