0690c3102efab94e6acfb811a443376b1b95641f
Andrea Miranda <a4mirand@uwaterloo.ca>
2023-07-15 17:30:55 -0400

    removed migrations folder

--- api/app.py
+++ api/app.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import flask_appbuilder
+import importlib
+import sys
 import json
 
 app = Flask(__name__)
@@ -13,58 +15,19 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow()
 
-
 class University(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    languages = db.Column(db.String(128))
-    terms = db.Column(db.String(128))
-    competition = db.Column(db.String(128))
-    program = db.Column(db.String(128))
-    location = db.Column(db.String(128))
-    courses = db.relationship('Course', backref='university')
-
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(128))
+	languages = db.Column(db.String(128))
+	terms = db.Column(db.String(128))
+	competition = db.Column(db.String(128))
+	program = db.Column(db.String(128))
+	location = db.Column(db.String(128))
 
 class UniversitySchema(ma.Schema):
     class Meta:
         # Fields to expose
         fields = ("program", "location", "languages", "terms", "competition")
-
-
-# Many to Many Relationship between UW Course and a Foreign University Course
-course_equivalency = db.Table('course_equivalency',
-                              db.Column("uw_course_id", db.Integer,
-                                        db.ForeignKey('course.id')),
-                              db.Column("foreign_course_id", db.Integer, db.ForeignKey('course.id')))
-"""
-Course Model
-"""
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    code = db.Column(db.String(20))
-    terms = db.Column(db.String(120))
-    university_id = db.Column(db.Integer, db.ForeignKey('university.id'))
-    description = db.Column(db.String(128), nullable=True)
-
-    foreign_courses = db.relationship(
-        "Course",
-        secondary=course_equivalency,
-        primaryjoin=id == course_equivalency.c.uw_course_id,
-        secondaryjoin=id == course_equivalency.c.foreign_course_id,
-        backref="uw_courses",
-    )
-
-
-class CourseSchema(ma.Schema):
-    foreign_courses = ma.Nested(lambda: CourseSchema(), many=True)
-    class Meta:
-        # Fields to expose
-        fields = ("name", "code", "terms", "foreign_courses", "university_id")
-
-
-course_schema = CourseSchema()
-courses_schema = CourseSchema(many=True)
 
 with app.app_context():
     db.create_all()
@@ -74,63 +37,24 @@ unis_schema = UniversitySchema(many=True)
 
 @app.route('/universities', methods=['GET'])
 def index():
-    unis = University.query.all()
-    res = unis_schema.dump(unis)
-    return jsonify(res)
-
+	unis = University.query.all()
+	res = unis_schema.dump(unis)
+	return jsonify(res)
 
 @app.route('/search_unis/<param>', methods=['GET'])
 def search_unis(param):
-    unis = University.query.filter(University.program.like('%'+param+'%') | University.location.like('%'+param+'%'))
-    res = unis_schema.dump(unis)
-    return jsonify(res)
+	unis = University.query.filter(University.program.like('%'+param+'%') | University.location.like('%'+param+'%'))
+	res = unis_schema.dump(unis)
+	return jsonify(res)
 
-@app.route('/university/<int:uni_id>/courses', methods=['GET'])
-def get_all_courses_for_university(uni_id):
-    university = University.query.filter_by(id =uni_id).first()  
-    data = courses_schema.dump(university.courses)
-    return jsonify(data)
 
-@app.route('/search_courses/<string:query>', methods=['GET'])
-def search_courses(query):
-    uw_uni = University.query.filter_by(name='University of Waterloo').first()
-
-    courses = Course.query.filter((Course.name.like('%'+query+'%') | Course.code.like('%'+query+'%')) & Course.university_id.like(uw_uni.id) ).all()
-    data = courses_schema.dump(courses)
-    return jsonify(data)
-
-@app.route('/uw/courses', methods=['GET'])
-def get_all_uw_courses():
-    uw_uni = University.query.filter_by(name='University of Waterloo').first()
-    courses = Course.query.filter(Course.university_id.like(uw_uni.id) ).all()
-
-    data = courses_schema.dump(courses)
-    return jsonify(data)
-
-@app.route('/courses', methods=['GET'])
-def get_all_courses():
-    courses = Course.query.all()
-    data = courses_schema.dump(courses)
-    return jsonify(data)
-
-@app.route('/course/<int:course_id>', methods=['GET'])
-def get_course_by_id(course_id):
-    courses = Course.query.filter_by(id =course_id).first()
-    data = course_schema.dump(courses)
-    return jsonify(data)
-	
-
-@app.route('/course/<string:course_name>', methods=['GET'])
-def get_courses_by_name(course_name):
-    courses = Course.query.filter(Course.name.like('%'+course_name+'%'))
-    data = courses_schema.dump(courses)
-    return jsonify(data)
-
-@app.route('/course/<int:course_id>/equivalency', methods=['GET'])
-def get_course_equivalencies(course_id):
-    course = Course.query.filter_by(id =course_id).first()  
-    data = courses_schema.dump(course.foreign_courses)
-    return jsonify(data)
+# @app.route('/get_uni/<name>', methods=['GET'])
+# def get_uni(name):
+# 	result = University.get_by_name(col, name)
+# 	if not result: return json_response("University not found", 400)
+# 	resultDct = dict(result)
+# 	resultDct.pop("_id")
+# 	return json_response(resultDct)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
