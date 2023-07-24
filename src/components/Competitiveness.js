59a56e3560996af8e80b79926ea65422d35c178b
aparajitaghimire <aparajitag16@gmail.com>
2023-07-24 16:45:59 -0400

    updated the competitveness to return transparent

--- src/components/Competitiveness.js
+++ src/components/Competitiveness.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Text} from "react-native";
+import { Text } from "react-native";
 import { Box, Stack } from "@mui/material";
 
 function Competitiveness(competition) {
-  return(
+  return (
     <Stack direction="row">
       <Box
         sx={{
@@ -12,19 +12,17 @@ function Competitiveness(competition) {
             if (comp == "ultra competitive") return "red";
             else if (comp == "very competitive") return "#ECE54B";
             else if (comp == "competitive") return "#48C246";
-            else return "blue";
+            else return "rgba(0, 0, 0, 0)";
           },
           borderRadius: "50%",
           height: "40px",
           width: "40px",
-          marginRight: "5px", 
+          marginRight: "5px",
         }}
       ></Box>
-      <Text style={{ margin: "10px", top: "50%" }}>
-        {competition}
-      </Text>
+      <Text style={{ margin: "10px", top: "50%" }}>{competition}</Text>
     </Stack>
-  )
+  );
 }
 
 export default Competitiveness;
