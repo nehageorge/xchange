6eb2e04e48bf3ba5d4bba0f1cded88692bbee459
ZhengmaoOuyang <ouy.zhengmao@gmail.com>
2023-07-25 22:17:12 -0400

    final changes

--- src/components/AddAReview.js
+++ src/components/AddAReview.js
@@ -1,62 +1,9 @@
 import "./AddAReview.css";
 import XChangeButton from "./XChangeButton.js";
+import CustomRating from "./CustomRating.js";
+
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-
 import { TextField, Select, MenuItem, Grid } from "@mui/material";
-
-import Rating from "@mui/material/Rating";
-import Circle from "@mui/icons-material/Circle";
-
-const StyledRating = styled(Rating)({
-  "& .MuiRating-iconFilled": {
-    color: "#8b8b8b",
-  },
-  "& .MuiRating-iconHover": {
-    color: "#8b8b8b",
-  },
-  "& .MuiRating-iconEmpty": {
-    color: "#d9d9d9",
-  },
-});
-
-function CustomizedRating(text) {
-  const [value, setValue] = React.useState(2);
-  const [hover, setHover] = React.useState(-1);
-
-  return (
-    <div style={{ maxHeight: 400, maxWidth: 300, paddingBottom: "1rem" }}>
-      <Typography component="legend">{text}</Typography>
-      <Box
-        sx={{
-          "& > legend": { mt: 2 },
-          display: "flex",
-          flexFlow: "row",
-          alignItems: "center",
-        }}
-      >
-        <StyledRating
-          name="customized-color"
-          defaultValue={2}
-          max={10}
-          getLabelText={(value) => `${value} Circle${value !== 1 ? "s" : ""}`}
-          precision={1}
-          icon={<Circle fontSize="inherit" />}
-          emptyIcon={<Circle fontSize="inherit" />}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-          onChangeActive={(event, newHover) => {
-            setHover(newHover);
-          }}
-        />
-        <div>{`${value} / 10`}</div>
-      </Box>
-    </div>
-  );
-}
 
 function textBox(txt) {
   return (
@@ -88,7 +35,7 @@ function Slct(txt) {
       value={slct}
       label={txt}
       onChange={handleChange}
-      style={{ width: "100%" }}
+      style={{ width: "100%", backgroundColor: "#ffffff" }}
     >
       <MenuItem value={10}>AAAA</MenuItem>
       <MenuItem value={20}>BBBB</MenuItem>
@@ -97,7 +44,12 @@ function Slct(txt) {
   );
 }
 
-function AddAReview() {
+function AddAReview(props) {
+
+  function close() {
+    props.setOpen(false);
+  }
+
   return (
     <div class="ratings-box">
       <div class="padding">
@@ -140,28 +92,28 @@ function AddAReview() {
             </div>
           </Grid>
           <Grid item xs={12}>
-            <p class="add-a-review">My experience was ...</p>
+            <p class="my-experience-was">My experience was ...</p>
           </Grid>
           <Grid item xs={6}>
-            <div>{CustomizedRating("Safe")}</div>
+            <div>{CustomRating("Safe")}</div>
           </Grid>
           <Grid item xs={6}>
-            <div>{CustomizedRating("Fun")}</div>
+            <div>{CustomRating("Fun")}</div>
           </Grid>
           <Grid item xs={6}>
-            <div>{CustomizedRating("Affordable")}</div>
+            <div>{CustomRating("Affordable")}</div>
           </Grid>
           <Grid item xs={6}>
-            <div>{CustomizedRating("Easy")}</div>
+            <div>{CustomRating("Easy")}</div>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={6}>
             <div></div>
           </Grid>
-          <Grid item xs={4}>
-            <div>{XChangeButton("Cancel")}</div>
+          <Grid item xs={3}>
+            <div onClick={close}>{XChangeButton("Cancel")}</div>
           </Grid>
-          <Grid item xs={4}>
-            <div>{XChangeButton("Save")}</div>
+          <Grid item xs={3}>
+            <div onClick={close}>{XChangeButton("Save")}</div>
           </Grid>
         </Grid>
       </div>
--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,63 +1,47 @@
 import "./Discussion.css"
 import XChangeButton from "./XChangeButton.js";
+import CustomRating from "./CustomRating.js";
+import AddAReview from "./AddAReview.js";
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
-import Rating from '@mui/material/Rating';
-import Circle from '@mui/icons-material/Circle';
+import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
 
 import List from '@mui/material/List';
 
-const StyledRating = styled(Rating)({
-  '& .MuiRating-iconFilled': {
-    color: '#8b8b8b',
-  },
-  '& .MuiRating-iconHover': {
-    color: '#8b8b8b',
-  },
-  '& .MuiRating-iconEmpty': {
-    color: '#d9d9d9',
-  },
-});
 
-function CustomizedRating(text) {
-
-    const [value, setValue] = React.useState(2);
-    const [hover, setHover] = React.useState(-1);
-
-  return (
-    <div style={{ maxHeight: 400, paddingBottom: "1rem" }}>
-        <Typography component="legend">{text}</Typography>
-        <Box
-        sx={{
-            '& > legend': { mt: 2 },
-            display: 'flex',
-            flexFlow: 'row',
-            alignItems: 'center',
-        }}
+function SimpleDialogDemo() {
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState('');
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (value) => {
+      setOpen(false);
+      setSelectedValue(value);
+    };
+  
+    return (
+      <div>
+        <div onClick={handleClickOpen}>
+            {XChangeButton("Add a Review")}
+        </div>
+        <Dialog
+          selectedValue={selectedValue}
+          open={open}
+          onClose={handleClose}
+          fullWidth={true}
+          maxWidth = {'md'}
         >
-        <StyledRating
-            name="customized-color"
-            defaultValue={2}
-            max={10}
-            getLabelText={(value) => `${value} Circle${value !== 1 ? 's' : ''}`}
-            precision={1}
-            icon={<Circle fontSize="inherit" />}
-            emptyIcon={<Circle fontSize="inherit" />}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            onChangeActive={(event, newHover) => {
-                setHover(newHover);
-            }}
-        />
-        <div>{`${value} / 10`}</div>
-        </Box>
-    </div>
-  );
-}
+            <AddAReview
+                setOpen={setOpen}
+            />
+        </Dialog>
+      </div>
+    );
+  }
 
 function Review() {
     return (
@@ -109,12 +93,12 @@ function Discussion() {
                 </div>
                 <div class='ratings-box'>
                     <div class='ratings'>
-                        {CustomizedRating("Safe")}
-                        {CustomizedRating("Fun")}
-                        {CustomizedRating("Affordable")}
-                        {CustomizedRating("Easy")}
+                        {CustomRating("Safe")}
+                        {CustomRating("Fun")}
+                        {CustomRating("Affordable")}
+                        {CustomRating("Easy")}
                         <div style={{ maxHeight: 20, paddingBottom: "1.5rem" }}></div>
-                        {XChangeButton("Add a Review")}
+                        {SimpleDialogDemo()}
                     </div>
                 </div>
             </div>
