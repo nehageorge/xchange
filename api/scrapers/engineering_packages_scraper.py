a951ec06785e0e3c2f56a23b6c56aeb317846f8c
Andrea Miranda <a4mirand@uwaterloo.ca>
2023-07-26 23:09:31 -0400

    get information from all university links on the engineering page

--- api/scrapers/engineering_packages_scraper.py
+++ api/scrapers/engineering_packages_scraper.py
@@ -33,46 +33,37 @@ for link in links:
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-links = list(links)
+    table = soup.find('table')
+    if table is None:
+        continue 
 
+    faculties = []
+    rows = table.findChildren('tr')
 
+    faculty = None
+    student = None 
 
-
-
-
-
-
-# page = requests.get("https://uwaterloo.ca/engineering/czech-technical-university-ctu-course-packages")
-
-# soup = BeautifulSoup(page.content, "html.parser")
-
-# table = soup.find('table')
-# # print(table)
-# faculties = []
-# rows = table.findChildren('tr')
-# print(f"Rows in table:\t{len(rows)}")
-# faculty = None
-# student = None 
-# for row in rows: 
-#     # The titles of the Engineering on the page have colspan = 2
-#     tds = row.find_all('td')
-#     if len(tds) == 1:
-#         td = tds[0]
-#         col_span = int(td.get("colspan", 0))
-#         if col_span == 2: 
-#             td_faculty = row.findChildren('h2')
-#             if len(td_faculty) == 1:
-#                 faculty = td_faculty[0].get_text() 
-#                 faculties.append(faculty)
-#                 print(f"{faculty}")
-#             # Student details are in a <td> that contains a <strong> tag
-#             td_student = row.findChildren('strong')
-#             if len(td_student) == 1:
-#                 student = td_student[0].get_text()
-#     elif len(tds) == 2:
-#         uwcourse = tds[0].get_text()
-#         foreign_course = tds[1].get_text()
-#         print(f"{student}\t\t{uwcourse}\t\t{foreign_course}\t\t{faculty}")
+    for row in rows: 
+        # The titles of the Engineering on the page have colspan = 2
+        tds = row.find_all('td')
+        if len(tds) == 1:
+            td = tds[0]
+            col_span = int(td.get("colspan", 0))
+            if col_span == 2: 
+                td_faculty = row.findChildren('h2')
+                if len(td_faculty) == 1:
+                    faculty = td_faculty[0].get_text() 
+                    faculties.append(faculty)
+                    print(f"{faculty}")
+                # Student details are in a <td> that contains a <strong> tag
+                td_student = row.findChildren('strong')
+                if len(td_student) == 1:
+                    student = td_student[0].get_text()
+        elif len(tds) == 2:
+            # Courses Taken by a student are in the table in <tr> elements with 2 <td> elements
+            uwcourse = tds[0].get_text()
+            foreign_course = tds[1].get_text()
+            print(f"{student}\t\t{uwcourse}\t\t{foreign_course}\t\t{faculty}")
 
                 
 
