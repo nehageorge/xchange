9b3ccacd997a898c978fa132cbb7b55a0bf7680d
AndreaMiranda99 <anne.andrea26@gmail.com>
2023-07-27 20:40:51 -0400

    update scraper to match CourseEquivalency Model

--- api/scrapers/engineering_packages_scraper.py
+++ api/scrapers/engineering_packages_scraper.py
@@ -19,14 +19,11 @@ def clean_space_and_commas(word):
 table = soup.find('table')
 universites = {}
 links = set()
-count = 0
-
 
 for td in table.find_all('td'):
     col_span = int(td.get("colspan", 0))
     if col_span == 0:
         for a in td.find_all('a', href=True):
-            count += 1
             link = a['href']
             links.add(link)
             university_name = a.get_text()
@@ -36,67 +33,59 @@ for td in table.find_all('td'):
 
 print(f"Engineering has information on {len(universites)} Universitites")
 
-# for a in table.find_all('a', href=True):
-#     count += 1
-#     link = a['href']
-#     links.add(link)
-#     university_name = a.get_text()
-#     university_name = re.sub(r'\([^)]*\)', '', university_name)
-#     print(f"{university_name}")
-#     universites[link] = university_name
+print(f"Pages with Course Packages at Universities: {len(links)}\n")
 
-# print(f"\n\nAll links in the table:\t{count}")
+with open('data/course_packages.csv', 'w') as file:
+    file.write(f"Program,UW Course,Foreign Course,Host University,Year Taken\n")
 
-# print(f"Pages with Course Packages at Universities: {len(links)}\n")
-# with open('course_packages.csv', 'w') as file:
-#     file.write(f"Student,UW Course,Foreign Course,Faculty\n")
-
-#     for link in links:
-#         print(f"Link:\t{link}")
-#         URL = ""
-#         if link[0] == '/': 
-#             URL = f"{UW_URL}{link}"
-#         elif link[0:5] == "https":
-#             URL = link
-#         elif link[0] == '#':
-#             continue
+    for link in links:
+        print(f"Link:\t{link}")
+        URL = ""
+        if link[0] == '/': 
+            URL = f"{UW_URL}{link}"
+        elif link[0:5] == "https":
+            URL = link
+        elif link[0] == '#':
+            continue
             
-#         page = requests.get(URL)
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-#         soup = BeautifulSoup(page.content, "html.parser")
+        table = soup.find('table')
+        if table is None:
+            continue 
 
-#         table = soup.find('table')
-#         if table is None:
-#             continue 
+        eng_programs = []
+        rows = table.findChildren('tr')
 
-#         faculties = []
-#         rows = table.findChildren('tr')
-
-#         faculty = None
-#         student = None 
+        program = None
+        year_taken = None 
         
+        for row in rows: 
+            # The titles of the Engineering on the page have colspan = 2
+            tds = row.find_all('td')
+            if len(tds) == 1:
+                td = tds[0]
+                col_span = int(td.get("colspan", 0))
+                if col_span == 2: 
+                    td_program = row.findChildren('h2')
+                    if len(td_program) == 1:
+                        program = clean_space_and_commas(td_program[0].get_text())
+                        eng_programs.append(program)
+                        # print(f"{program}")
+                    # Student details are in a <td> that contains a <strong> tag
+                    td_student = row.findChildren('strong')
+                    if len(td_student) == 1:
+                        year_match = re.search(r'\b(19|20)\d{2}\b', td_student[0].get_text()) 
+                        if year_match:
+                            year_taken = year_match.group(0)
 
-#         for row in rows: 
-#             # The titles of the Engineering on the page have colspan = 2
-#             tds = row.find_all('td')
-#             if len(tds) == 1:
-#                 td = tds[0]
-#                 col_span = int(td.get("colspan", 0))
-#                 if col_span == 2: 
-#                     td_faculty = row.findChildren('h2')
-#                     if len(td_faculty) == 1:
-#                         faculty = clean_space_and_commas(td_faculty[0].get_text())
-#                         faculties.append(faculty)
-#                         print(f"{faculty}")
-#                     # Student details are in a <td> that contains a <strong> tag
-#                     td_student = row.findChildren('strong')
-#                     if len(td_student) == 1:
-#                         student = clean_space_and_commas(td_student[0].get_text())
-#             elif len(tds) == 2:
-#                 # Courses Taken by a student are in the table in <tr> elements with 2 <td> elements
-#                 uwcourse = clean_space_and_commas(tds[0].get_text())
-#                 foreign_course = clean_space_and_commas(tds[1].get_text())
-#                 print(f"{student}\t\t{uwcourse}\t\t{foreign_course}\t\t{faculty}")
-#                 if faculty and student: 
-#                     file.write(f"{faculty},{uwcourse},{foreign_course},{student}\n")
+            elif len(tds) == 2:
+                # Courses Taken by a year_taken are in the table in <tr> elements with 2 <td> elements
+                uwcourse = clean_space_and_commas(tds[0].get_text())
+                foreign_course = clean_space_and_commas(tds[1].get_text())
+                print(f"{program}\t{uwcourse}\t{foreign_course}\t{universites[link]}\t{year_taken}")
+                if program and year_taken: 
+                    file.write(f"{program},{uwcourse},{foreign_course},{universites[link]},{year_taken}\n")
+                
 
