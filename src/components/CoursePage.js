74e0b17bf95329103f8993d814066e1cff4e3f4a
nehageorge <55307539+nehageorge@users.noreply.github.com>
2023-08-16 14:51:20 -0400

    fix prev course equivalencies (#62)
    * fix prev course equivalencies
    
    * remove imports

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,8 +1,7 @@
 import "./CoursePage.css";
 import { Text, View } from "react-native";
 import React, { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
-import { TableCell, TableRow } from "@mui/material";
+import { useParams } from "react-router-dom";
 import XchangeTabbedHeader from "./XchangeTabbedHeader";
 import XchangeCourseTable from "./XchangeCourseTable";
 
@@ -14,13 +13,12 @@ function CoursePage() {
   const [query, setQuery] = useState("");
   useEffect(() => {
     fetch("/course/" + params.id).then((res) =>
-    res.json().then((data) => {
-      setCourse(data);
-    })
-  );
+      res.json().then((data) => {
+        setCourse(data);
+      })
+    );
 
-    fetch("/course_equivalencies", {
-    }).then((res) =>
+    fetch("/course_equivalencies", {}).then((res) =>
       res.json().then((data) => {
         setCoursesEquivalency(data);
         setAllCoursesEquivalency(data);
@@ -41,24 +39,36 @@ function CoursePage() {
           }}
         ></img>
       </View>
-      <View style={{ flex: 1, padding: 20, marginLeft: "30px", marginRight: "50px" }}>
+      <View
+        style={{
+          flex: 1,
+          padding: 20,
+          marginLeft: "30px",
+          marginRight: "50px",
+        }}
+      >
         <Text style={{ fontSize: 20, fontWeight: "bold", textAlign: "left" }}>
-            {course["code"]} : {course["name"]}
+          {course["code"]} : {course["name"]}
         </Text>
-        <hr style={{opacity: "100%", width: "25%", background: "#E0D03B", height: "3px", border: "none"}}/>  
-        <Text>{course["description"]}
-        </Text>
+        <hr
+          style={{
+            opacity: "100%",
+            width: "25%",
+            background: "#E0D03B",
+            height: "3px",
+            border: "none",
+          }}
+        />
+        <Text>{course["description"]}</Text>
         <br></br>
         <XchangeCourseTable
-            headers={[
-                "Previously Approved Courses",
-            ]}
-            colWidths={["40%"]}
+          headers={["Previously Approved Courses"]}
+          colWidths={["40%"]}
+          course_id={params.id}
         />
-
       </View>
     </div>
   );
 }
-  
+
 export default CoursePage;
